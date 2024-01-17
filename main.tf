f0ff4f4fd3ccde82a6a8f566857b2180f510f27a
Will <yzj_000@163.com>
2024-01-17 09:35:08 +0800

    Change infrastructure

--- main.tf
+++ main.tf
@@ -16,4 +16,16 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "eastasia"
+
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team = "DevOps"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  address_space       = ["10.0.0.0/16"]
+  location            = "eastasia"
+  name                = "myTFVnet"
+  resource_group_name = azurerm_resource_group.rg.name
 }
